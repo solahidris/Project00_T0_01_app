5782f4ec0904a985a0b218565682a4b6feec7ccc
solahidris <emailsolah@gmail.com>
2023-02-25 17:55:28 +0900

    trying to commit

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React from "react";
+import Card from "../UI/Card";
+import classes from './AddUser.module.css';
+import Button from '../UI/Button';
 
-const AddUser = props => {
-    const addUserHandler = (event) => {
-        event.preventDefault();
-    };
+const AddUser = (props) => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
 
-    return (
-        <form onSubmit={addUserHandler}>
-            <label htmlFor="username">Username</label>
-            <input id="username" type="text"></input>
-            <label htmlFor="age">Age (Years)</label>
-            <input id="age" type="number"></input>
-            <button type="submit">Add User</button>
-        </form>
-    );
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text"></input>
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number"></input>
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
 };
 
 export default AddUser;
